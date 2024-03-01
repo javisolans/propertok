6e7ab5019870e78b2289527e8734fac52940e514
Javier Solans <javier.solans7@gmail.com>
2024-03-01 12:11:57 +0100

    save

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
                 rel="noopener noreferrer"
               >
                 <Image
-                  src="/logo_1.jpg"
+                  src="/logo_1.jpeg"
                   alt="Propertok Logo"
                   className="dark:invert"
                   width={60}
