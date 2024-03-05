f3544b15f3e985ab39c389a75803ad5e15ba2e36
Javier Solans <javier.solans7@gmail.com>
2024-03-05 19:23:51 +0100

    save

--- app/page.tsx
+++ app/page.tsx
@@ -9,42 +9,55 @@ export default function Home() {
           <div className="max-w-xl">
             <div className="flex pt-10 pb-6 w-full justify-center lg:static lg:h-auto lg:w-auto lg:bg-none">
               <a
-                className="pointer-events-none flex place-items-center gap-2 lg:pointer-events-auto lg:p-0"
+                className="pointer-events-none flex place-items-center gap-2 lg:pointer-events-auto lg:p-0 align-middle"
                 rel="noopener noreferrer"
               >
                 <Image
-                  src="/logo_1.jpeg"
+                  src="/logo_1.png"
                   alt="Propertok Logo"
                   className="dark:invert"
-                  width={60}
+                  width={80}
                   height={24}
                   priority
                 />
               </a>
+              <h1 className="text-5xl font-bold text-black align-middle pt-4 pl-6">
+                Propertok
+              </h1>
             </div>
-            <h1 className="text-5xl font-bold text-primary">Propertok</h1>
 
-            <h2 className="py-6 text-5xl font-bold w-full">
-              Accede a los inmuebles que no están en Idealista
+            <h2 className="py-6 text-5xl font-extrabold w-full">
+              Discover homes from TikTok. Simple.
             </h2>
+
+            <ul className="text-justify list-disc	pb-10">
+              <li className="p-2">
+                Intuitive and user-friendly interface that simplifies TikTok
+                property searches and saves time.
+              </li>
+              <li className="p-2">
+                Real-time notifications for new listings to never miss a
+                potential opportunity.
+              </li>
+              <li className="p-2">
+                Customizable tagging and categorization features that allow to
+                organize properties efficiently and effectively.
+              </li>
+            </ul>
+
             <p className="pb-6">
-              El 80% de las propiedades no son anunciadas en los portales
-              principales. Con Propertok te damos acceso a las que se venden a
-              través de TikTok para que tengas un nuevo canal de compra.
-            </p>
-            <p className="pb-6">
-              ¡Apúntate para recibir notificaciones cuando lancemos Propertok y
-              aprovecha una OFERTA DE LANZAMIENTO LIMITADA ⤵️!
+              Sign up to receive notifications when we launch Propertok and take
+              advantage of a LIMITED LAUNCH OFFER ⤵️!
             </p>
 
             <div>
               <Link href="https://tally.so/r/w8QMvz">
-                <button className="btn btn-primary">Avísame</button>
+                <button className="btn btn-primary text-white">
+                  Sign me in!
+                </button>
               </Link>
             </div>
-            <p className="pt-2 pb-6">
-              3 personas suscritas
-            </p>
+            <p className="pt-2 pb-6 text-primary">10 people signed in</p>
             {/* <label className="form-control w-full ">
               <input
                 type="text"
@@ -55,30 +68,15 @@ export default function Home() {
                 <button className="btn btn-primary">Avísame</button>
               </div>
             </label> */}
-            <h2 className="pt-6 text-2xl font-bold w-full">
-              Inmuebles en Tiktok?
-            </h2>
-            <p className="py-6">
-              El 80% de los inmuebles vendidos en España lo son fuera de los
-              portales inmobiliarios. TikTok se ha convertido en un gran espacio
-              generador de transacciones inmobiliarias. En Propertok, buscamos
-              facilitar y organizar el acceso a este tipo de inmuebles.
+            <h2 className="pt-6 text-2xl font-bold w-full">Homes on Tiktok?</h2>
+            <p className="py-6 text-justify">
+              Did you know that 80% of property sales happen outside of
+              traditional real estate websites? TikTok has emerged as a major
+              hub for real estate transactions. Here at Propertok, we are
+              dedicated to simplifying and streamlining access to these kinds of
+              properties.
             </p>
-            <div className="border-2 rounded-3xl py-2">
-              <a
-                className="justify-center pointer-events-none flex place-items-center gap-2 py-8 lg:pointer-events-auto lg:p-0"
-                rel="noopener noreferrer"
-              >
-                <Image
-                  src="/noticia2.png"
-                  alt="Propertok Logo"
-                  className="dark:invert"
-                  width={500}
-                  height={24}
-                  priority
-                />
-              </a>
-            </div>
+
             <div className="pt-6">
               <div className="border-2 rounded-3xl">
                 <a
@@ -119,7 +117,7 @@ export default function Home() {
 
       <p className="pb-20">
         Propertok is being developed by{" "}
-        <a href="https://twitter.com/sopadesigns">@sopadesigns</a>.
+        <a href="https://twitter.com/sopadesigns">@sopadesigns</a>
       </p>
     </main>
   );
